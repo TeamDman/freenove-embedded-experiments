df6ffc97fd95804f59857c5f906c890abc253ad2
TeamDman <TeamDman9201@gmail.com>
2025-04-23 16:51:02 -0400

    blinking one

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use std::time::Duration;
 
-    // Bind the log crate to the ESP Logging facilities
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+use eyre::bail;
+
+fn sleep(duration: Duration) {
+    FreeRtos::delay_ms(duration.as_millis() as u32);
+}
+
+fn main() -> eyre::Result<()> {
+    esp_idf_hal::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
-    log::warn!("bruh!");
+    color_eyre::install()?;
+
+    let peripherals = Peripherals::take()?;
+    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+    let mut i = 0;
+
+    let high_duration = Duration::from_millis(1000);
+    let low_duration = Duration::from_millis(1000);
+    loop {
+        led.set_high()?;
+        // we are sleeping here to make sure the watchdog isn't triggered
+        sleep(high_duration);
+
+        led.set_low()?;
+        sleep(low_duration);
+
+        log::info!("BRUH! {i}");
+        i+=1;
+        if i > 10 {
+            bail!("Reached 10!");
+        }
+    }
 }
